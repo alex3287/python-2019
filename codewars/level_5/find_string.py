e8465973de9347a5cb757da6ea4baef5b7e75468
Alexander Mangazeev <alex3287@bk.ru>
2019-01-26 09:13:02 +0700

    version_2 26.01.19

--- codewars/level_5/find_string.py
+++ codewars/level_5/find_string.py
@@ -1,21 +1,20 @@
 def my_sort(s):
-    s = set(s)
-    string = (sorted(s.lower()))
+
+    m = set(s.strip().lower())
+    string = sorted(m)
+    string = ''.join(string)
     return string
 
+
 def find_uniq(arr):
-    one = my_sort(arr[0].strip())
-    two = my_sort(arr[1].strip())
-    l = len(one)
-    if len(two)<len(one):
-        l = len(two)
-    l+=1
-    if one[:l] == two[:l]:
-        for i in arr:
-            if my_sort(i)[:l] != one[:l]:
-                return i
-    return 's'
+    D = {}
+    for i in arr:
+        t = my_sort(i)
+        D.setdefault(t,0)
+        D[t] += 1
+
+    return print(min(D.values()))
 
 if __name__ == "__main__":
-    s ='aSdfA'
-    print(sorted(s.lower()))
+    s =['aSdfA ','rty','aasdf','asdf']
+    print(find_uniq(s))
