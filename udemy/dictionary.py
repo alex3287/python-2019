8998169b28c6c7fa640a651ad5f140ab0bb5222f
Alexander Mangazeev <alex3287@bk.ru>
2020-01-04 19:30:53 +0700

    task from course

---
+++ udemy/dictionary.py
@@ -0,0 +1,7 @@
+D = {'one': 1, 'two': 2, 'three':3}
+print(D['two'])
+
+Computer = {'monitor': 'output device', 'mouse': 'manipulator',
+            'keyboard': 'main input device', 'CPU': 'computer brain',
+            'HDD': 'information storage device'}
+print(Computer['HDD'])
